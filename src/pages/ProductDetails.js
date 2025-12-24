9751f597b2bc0c12c41202b267a9c66498f05fd8
Kritish <iminati921@gmail.com>
2025-12-24 02:16:27 +0100

    Update ProductDetails.js

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext, useCallback } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { CartContext } from "../context/CartContext";
 import { supabase } from "../supabase";
