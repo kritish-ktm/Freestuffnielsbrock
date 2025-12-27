93caf07400e88d22c1975911cfe5f35dd109b787
Kritish <iminati921@gmail.com>
2025-12-27 02:31:02 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,22 +8,22 @@ import {
   onAuthStateChanged 
 } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";  // CORRECT
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDR2rSXwGgfoqJszPCnB6-USVB4FnR89dc",
-  authDomain: "free-stuff-nielsbrock.firebaseapp.com",
-  projectId: "free-stuff-nielsbrock",
-  storageBucket: "free-stuff-nielsbrock.firebasestorage.app",
-  messagingSenderId: "622024988493",
-  appId: "1:622024988493:web:d27f3f45b6b6e805a10904"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
-const storage = getStorage(app);  // WORKS
+const storage = getStorage(app);
 
 googleProvider.setCustomParameters({
   prompt: "select_account"
@@ -32,7 +32,7 @@ googleProvider.setCustomParameters({
 export {
   auth,
   db,
-  storage,  // EXPORTED
+  storage,
   googleProvider,
   signInWithPopup,
   signOut,
