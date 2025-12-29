e8ae073a4bf4557731b603476f2b2ca43d8e9631
Kritish <iminati921@gmail.com>
2025-12-29 23:07:05 +0100

    Update supabase.js

--- FreestuffNielsbrock/src/supabase.js
+++ FreestuffNielsbrock/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://crcmzrtbuyahubqhutsz.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyY216cnRidXlhaHVicWh1dHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MTYxNzcsImV4cCI6MjA1MTM5MjE3N30.eyjpc3NiIxNHViMWFzZSIsInNhSiIxOZSBHmYXVicj";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyY216cnRidXlhaHVicWh1dHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTU1MDcsImV4cCI6MjA4MTI5MTUwN30.x3BMvXQdbBCDnIXlGDvmIEAZUwZ1yHncxW7bHA0mHMU";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
