e88a1ffca4fa5a5095d0b339dc839cd9125269b1
Kritish Upadhyaya <iminati921@gmail.com>
2025-12-29 23:20:11 +0100

    Supabase Updates

--- FreestuffNielsbrock/src/supabase.js
+++ FreestuffNielsbrock/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://crcmzrtbuyahubqhutsz.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyY216cnRidXlhaHVicWh1dHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTU1MDcsImV4cCI6MjA4MTI5MTUwN30.x3BMvXQdbBCDnIXlGDvmIEAZUwZ1yHncxW7bHA0mHMU";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
