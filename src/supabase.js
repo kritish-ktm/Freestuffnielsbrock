8e64d785fff0d62cc324a931d17b2baa142f8116
Kritish <iminati921@gmail.com>
2025-12-29 18:51:02 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
-// src/supabase.js
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://crcmzrtbuyahubqhutsz.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyY216cnRidXlhaHVicWh1dHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTU1MDcsImV4cCI6MjA4MTI5MTUwN30.x3BMvXQdbBCDnIXlGDvmIEAZUwZ1yHncxW7bHA0mHMU";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+   
+   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+   const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+   
+   export const supabase = createClient(supabaseUrl, supabaseAnonKey);
